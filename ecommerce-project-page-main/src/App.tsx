9eea8b288a82bc50cd684451abfede9901f775bd
jdwilkin4 <jwilkin4@hotmail.com>
2023-09-18 21:34:12 -0700

    feat: setting up mobile carousel ecommerce app

--- ecommerce-project-page-main/src/App.tsx
+++ ecommerce-project-page-main/src/App.tsx
@@ -8,29 +8,42 @@ import { CartModal } from "./components/CartModal";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
+  const totalProductImages = 4;
   const isMobile = useMediaQuery();
   const [productCount, setProductCount] = useState(0);
   const [showCartModal, setShowCartModal] = useState(false);
+  const [currentThumbnailImg, setCurrentThumbnailImg] = useState(0);
 
-  const handleShowCartModal = () => {
-    setShowCartModal(!showCartModal);
-  };
+  const handleShowCartModal = () => setShowCartModal(!showCartModal);
 
   const handleUpdateCart = () => {
     console.log(productCount);
   };
 
-  const handleIncrementProductCount = () => {
-    setProductCount((prev) => prev + 1);
-  };
+  const handleIncrementProductCount = () => setProductCount((prev) => prev + 1);
 
   const handleDecrementProductCount = () => {
     if (productCount === 0) return;
     setProductCount((prev) => prev - 1);
   };
 
-  const handleRemoveItemsFromCart = () => {
-    setProductCount(0);
+  const handleRemoveItemsFromCart = () => setProductCount(0);
+
+  const handleShowNextThumbnailImg = () => {
+    if (currentThumbnailImg === totalProductImages - 1) {
+      setCurrentThumbnailImg(0);
+      return;
+    }
+
+    setCurrentThumbnailImg((prev) => prev + 1);
+  };
+
+  const handleShowPreviousThumbnailImg = () => {
+    if (currentThumbnailImg === 0) {
+      setCurrentThumbnailImg(totalProductImages - 1);
+      return;
+    }
+    setCurrentThumbnailImg((prev) => prev - 1);
   };
 
   return (
@@ -48,7 +61,11 @@ function App() {
             productCount={productCount}
             handleShowCartModal={handleShowCartModal}
           />
-          <MobileCarousel />
+          <MobileCarousel
+            handleShowPreviousThumbnailImg={handleShowPreviousThumbnailImg}
+            currentImage={currentThumbnailImg}
+            handleShowNextThumbnailImg={handleShowNextThumbnailImg}
+          />
         </>
       ) : (
         <>
--- ecommerce-project-page-main/src/components/MobileCarousel.tsx
+++ ecommerce-project-page-main/src/components/MobileCarousel.tsx
@@ -1,14 +1,33 @@
-export function MobileCarousel() {
+import "./MobileCarousel.scss";
+
+type MobileCarouselProps = {
+  currentImage: number;
+  handleShowNextThumbnailImg: () => void;
+  handleShowPreviousThumbnailImg: () => void;
+};
+
+export function MobileCarousel({
+  currentImage,
+  handleShowNextThumbnailImg,
+  handleShowPreviousThumbnailImg,
+}: MobileCarouselProps) {
   return (
     <div className="mobile-carousel">
-      <button>
+      <button
+        className="mobile-carousel__prev-btn carousel-btn"
+        onClick={handleShowPreviousThumbnailImg}
+      >
         <img src="./images/icon-previous.svg" alt="" />
       </button>
-      <img src="./images/image-product-1.jpg" alt="" />
-      <img src="./images/image-product-2.jpg" alt="" />
-      <img src="./images/image-product-3.jpg" alt="" />
-      <img src="./images/image-product-4.jpg" alt="" />
-      <button>
+      <img
+        className="mobile-carousel__featured-img"
+        src={`./images/image-product-${currentImage + 1}.jpg`}
+        alt={`shoe product thumbnail ${currentImage + 1}`}
+      />
+      <button
+        className="mobile-carousel__next-btn carousel-btn"
+        onClick={handleShowNextThumbnailImg}
+      >
         <img src="./images/icon-next.svg" alt="" />
       </button>
     </div>
