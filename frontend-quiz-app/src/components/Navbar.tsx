de3be5f3b2c462830fad653d81f2c45bfd8d8388
jdwilkin4 <jwilkin4@hotmail.com>
2023-12-05 20:39:46 -0800

    feat: adding basic layout for navbar quiz

---
+++ frontend-quiz-app/src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import "./Navbar.scss";
+
+type NavbarProps = {
+  quizTitle: string;
+  quizIcon: string;
+  isStartScreenShowing: boolean;
+};
+
+export default function Navbar({
+  quizIcon,
+  quizTitle,
+  isStartScreenShowing,
+}: NavbarProps) {
+  return (
+    <nav className="flex-container">
+      <div className="icon-container flex-container">
+        {isStartScreenShowing && (
+          <>
+            <img src={quizIcon} alt={`${quizTitle} icon`} />
+            <p>{quizTitle}</p>
+          </>
+        )}
+      </div>
+
+      <div className="theme-switch-wrapper flex-container">
+        {/*TODO: add light/dark logic*/}
+        <img src="/assets/images/icon-sun-dark.svg" alt="sun icon" />
+        <label
+          className="theme-switch-wrapper__theme-switch-label"
+          htmlFor="checkbox"
+        >
+          <input type="checkbox" id="checkbox" />
+          <div className="slider round"></div>
+        </label>
+        <img src="/assets/images/icon-moon-dark.svg" alt="moon icon" />
+      </div>
+    </nav>
+  );
+}
